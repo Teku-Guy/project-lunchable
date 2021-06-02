d6864251ab3a62f9d67c859f6900c77546303bf1
brittaniserna14 <brittaniserna14@gmail.com>
2021-06-01 17:50:17 -0700

    modal info and added grid modifications

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const sessionType = document.getElementById("sessionType");
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
